1ae2f2b6ce748fe396b9eb0908b9e8ab3706dde6
KhalilMohammad <khalilmohammadmirza@gmail.com>
2020-10-01 19:43:11 +0500

    Updates

---
+++ precache-manifest.a36f9851e61384a5c69a4594c92aae5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66d4a027f5ee62f3a391ca4631e4fa8c",
+    "url": "/Snippet-Gen/index.html"
+  },
+  {
+    "revision": "ae4e3b40d3f856676f52",
+    "url": "/Snippet-Gen/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "684a8ac70fb2fcff1920",
+    "url": "/Snippet-Gen/static/css/main.fff56229.chunk.css"
+  },
+  {
+    "revision": "ae4e3b40d3f856676f52",
+    "url": "/Snippet-Gen/static/js/2.bc10003e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Snippet-Gen/static/js/2.bc10003e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "684a8ac70fb2fcff1920",
+    "url": "/Snippet-Gen/static/js/main.e70763f7.chunk.js"
+  },
+  {
+    "revision": "56979f8f7d08f6f4fa0e",
+    "url": "/Snippet-Gen/static/js/runtime-main.dd87f21b.js"
+  }
+]);
